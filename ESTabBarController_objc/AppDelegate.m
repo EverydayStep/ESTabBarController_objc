56ceb9660c8db71def67d3f4110b4262ccb1b161
codeLocker <codeLockers@outlook.com>
2017-09-22 10:37:04 +0800

    适配xcode 9

--- ESTabBarController_objc/AppDelegate.m
+++ ESTabBarController_objc/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
+#import "ESTabBarController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +17,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    ViewController *vc = [[ViewController alloc] init];
+    ESTabBarController *tabVc = [[ESTabBarController alloc] init];
+    [tabVc setViewController:vc title:@"1" normalImage:nil selectedImage:nil];
+    self.window.rootViewController = tabVc;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
